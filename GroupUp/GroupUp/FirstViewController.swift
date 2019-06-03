f7d92fd68e04ad61c282392c1a0770b4be3d0233
etudiant <etudiant@iMac-de-etudiant.local>
2019-06-03 10:20:31 +0200

    Base

---
+++ GroupUp/GroupUp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GroupUp
+//
+//  Created by etudiant on 03/06/2019.
+//  Copyright © 2019 etudiant. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
