4c5d6bd46207f880f7373e4f45748355ca79b849
etudiant <etudiant@iMac-de-etudiant.local>
2019-06-04 11:09:40 +0200

    fix

--- GroupUp/GroupUp/FirstViewController.swift
+++ GroupUp/GroupUp/FirstViewController.swift
@@ -10,30 +10,9 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
-    @IBOutlet weak var menu: UIView!
-    @IBOutlet weak var menuButton: UIButton!
-    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        menu.layer.shadowOpacity = 1;
-        menu.layer.shadowRadius = 6;
-    }
-
-    @IBAction func openMenu(_ sender: Any) {
-        if leadingConstraint.constant == -150{
-            menuButton.imageView?.image = UIImage(named:"closeMenu");
-            leadingConstraint.constant = 0;
-            UIView.animate(withDuration: 0.5, animations: {
-                self.view.layoutIfNeeded();
-            });
-        }else{
-            menuButton.imageView?.image = UIImage(named:"openMenu");
-            leadingConstraint.constant = -150;
-            UIView.animate(withDuration: 0.5, animations: {
-                self.view.layoutIfNeeded();
-            });
-        }
     }
     
 }
