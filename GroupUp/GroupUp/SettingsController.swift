d113dfb3a995599afd0f0c1298c17ca1980eb498
etudiant <etudiant@iMac-de-etudiant-53.local>
2019-06-24 11:48:49 +0200

    avan

---
+++ GroupUp/GroupUp/SettingsController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsController.swift
+//  GroupUp
+//
+//  Created by etudiant on 24/06/2019.
+//  Copyright © 2019 etudiant. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+    /*@IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var distanceAlert: UILabel!
+    @IBOutlet weak var lengthSharing: UITextField!*/
+    
+    let step: Float = 20;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        
+    }
+    
+    
+    /*@IBAction func distanceValueChanged(_ sender: UISlider) {
+        let roundedValue = round(sender.value/step) * step
+        sender.value = roundedValue
+        distanceAlert.text = "\(roundedValue) m";
+    }*/
+    
+}
---
+++ GroupUp/GroupUp/SettingsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsViewController.swift
+//  GroupUp
+//
+//  Created by etudiant on 24/06/2019.
+//  Copyright © 2019 etudiant. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var distanceAlert: UILabel!
+    
+    let step: Float = 100;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        distanceAlert.text = String(0) + " m";
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+         let roundedValue = round(sender.value/step) * step
+         sender.value = roundedValue
+         distanceAlert.text = "\(roundedValue) m";
+    }
+    
+
+}
+
